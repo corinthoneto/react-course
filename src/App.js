1c1f45ea7fb34b3dc8e0f00026060faa0c91be2a
Corintho Fernandes <corintho@prodam.am.gov.br>
2018-08-28 16:05:26 -0400

    secao 3 aula 47 - aplicando css styles

--- src/App.js
+++ src/App.js
@@ -38,11 +38,21 @@ class App extends Component {
   }
 
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+
     return (
       <div className="App">
         <h1>Hello, I'm a React App</h1>
         <p>This is really works!!</p>
-        <button onClick={() => this.switchNameHandler('Fernandes')}>Switch Name</button>
+        <button style={style} onClick={() => this.switchNameHandler('Fernandes')}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
 
         <Person name={this.state.persons[1].name} 
